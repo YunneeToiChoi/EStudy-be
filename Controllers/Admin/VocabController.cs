490f899f535f02dc9d282bc5c57bfb24b8c5b3fa
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-06-15 18:21:50 +0700

    api new : Listen picture

--- Controllers/Admin/VocabController.cs
+++ Controllers/Admin/VocabController.cs
@@ -16,9 +16,11 @@ namespace study4_be.Controllers.Admin
     {
         private readonly ILogger<VocabController> _logger;
         private FireBaseServices _firebaseServices;
+        private GeneralAiAudioServices _generalAiAudioServices;
         public VocabController(ILogger<VocabController> logger, FireBaseServices firebaseServices)
         {
             _logger = logger;
+            _generalAiAudioServices = new GeneralAiAudioServices();
             _firebaseServices = firebaseServices;
         }
         private readonly VocabRepository _vocabsRepository = new VocabRepository();
@@ -89,10 +91,10 @@ namespace study4_be.Controllers.Admin
             var responseData = new List<VocabListenChoosenResponse>();
                 // Generate and upload audio to Firebase Storage
                 var audioFilePath = Path.Combine(Path.GetTempPath(), $"{vocabViewModel.vocab.VocabId}.wav");
-                GenerateAudio(vocabViewModel.vocab.VocabTitle, audioFilePath);
+                _generalAiAudioServices.GenerateAudio(vocabViewModel.vocab.VocabTitle, audioFilePath);
 
                 var audioBytes = System.IO.File.ReadAllBytes(audioFilePath);
-                var audioUrl = await UploadFileToFirebaseStorageAsync(audioBytes, $"{vocabViewModel.vocab.VocabId}.wav", firebaseBucketName);
+                var audioUrl = await _generalAiAudioServices.UploadFileToFirebaseStorageAsync(audioBytes, $"{vocabViewModel.vocab.VocabId}.wav", firebaseBucketName);
                 // Delete the temporary file after uploading
                 System.IO.File.Delete(audioFilePath);
             try
@@ -153,50 +155,6 @@ namespace study4_be.Controllers.Admin
         public IActionResult Vocab_Details()
         {
             return View();
-        }
-        // Thực hiện tải tệp lên Firebase Storage
-        public async Task<string> UploadFileToFirebaseStorageAsync(byte[] fileBytes, string fileName, string bucketName)
-        {
-            // Assuming your service account file is named "serviceAccount.json"
-            string serviceAccountPath = Path.Combine(Directory.GetCurrentDirectory(), "firebase_config.json");
-
-            // Load the credential from the file
-            var credential = GoogleCredential.FromFile(serviceAccountPath);
-
-            // Create a StorageClient object
-            var storage = StorageClient.Create(credential);
-            string correctedBucketName = "estudy-426108.appspot.com"; // Assuming the correct name is 'estudy426108'
-            // Create a MemoryStream object from the file bytes
-            using (var memoryStream = new MemoryStream(fileBytes))
-            {
-                // Upload the file to Firebase Storage
-                try
-                {
-                    var storageObject = await storage.UploadObjectAsync(correctedBucketName, fileName, null, memoryStream);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error uploading file: {ex.Message}");
-                }
-                return $"https://firebasestorage.googleapis.com/v0/b/{correctedBucketName}/o/{fileName}?alt=media";
-            }
-        }
-        private void GenerateAudio(string text, string filePath)
-        {
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = @"C:\Program Files (x86)\eSpeak NG\espeak-ng.exe",
-                Arguments = $"-w \"{filePath}\" \"{text}\"",
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            using (var process = new Process { StartInfo = startInfo })
-            {
-                process.Start();
-                process.WaitForExit();
-            }
         }
     }
 }
---
+++ Services/Response/ListenPictureResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace study4_be.Services.Response
+{
+    public class ListenPictureResponse
+    {
+        public int QuestionId { get; set; }
+        public string? QuestionAudio { get; set; }
+        public string? QuestionParagraph { get; set; }
+        public string? QuestionTranslate { get; set; }
+        public string? QuestionImage { get; set; }
+        public string? CorrectAnswer { get; set; }
+        public string? OptionA { get; set; }
+        public string? OptionB { get; set; }
+        public string? OptionC { get; set; }
+        public string? OptionD { get; set; }
+    }
+}
