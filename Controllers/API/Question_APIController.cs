508125571138a87b19b709bd09d8c4fd2f3473b2
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-06-15 18:45:24 +0700

    new api : listen question response

--- Controllers/API/Question_APIController.cs
+++ Controllers/API/Question_APIController.cs
@@ -90,6 +90,46 @@ namespace study4_be.Controllers.API
                 return StatusCode(500, new { status = 500, message = "An error occurred while processing your request." });
             }
         }
+        [HttpPost("Get_AllListen_Quest_Res")]
+        public async Task<IActionResult> Get_AllListen_Quest_Res(OfLessonRequest _req)
+        {
+            if (_req.lessonId == null)
+            {
+                _logger.LogWarning("LessonId is null or empty in the request.");
+                return BadRequest(new { status = 400, message = "LessonId is null or empty" });
+            }
+
+            try
+            {
+                var lessonTag = await _context.Lessons
+                         .Where(l => l.LessonId == _req.lessonId)
+                         .Select(l => l.Tag)
+                         .FirstAsync();
+                var lessonTagResponse = new
+                {
+                    lessonTag = lessonTag.TagId
+                };
+                var allQuestionOfLesson = await _questionRepo.GetAllQuestionsOfLesson(_req.lessonId);
+                var listenPictureResponse = allQuestionOfLesson.Select(question => new ListenPictureResponse
+                {
+                    QuestionId = question.QuestionId,
+                    QuestionAudio = question.QuestionAudio,
+                    QuestionImage = question.QuestionImage,
+                    QuestionParagraph = question.QuestionParagraph,
+                    QuestionTranslate = question.QuestionTranslate,
+                    CorrectAnswer = question.CorrectAnswer,
+                    OptionA = question.OptionA,
+                    OptionB = question.OptionB,
+                    OptionC = question.OptionC,
+                });
+                return Json(new { status = 200, message = "Get All Question Of Lesson Successful", data = listenPictureResponse, lessonTag = lessonTagResponse });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while fetching vocab for lesson {LessonId}", _req.lessonId);
+                return StatusCode(500, new { status = 500, message = "An error occurred while processing your request." });
+            }
+        }
 
     }
 }
---
+++ Services/Response/ListenQuestResResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace study4_be.Services.Response
+{
+    public class ListenQuestResResponse
+    {
+        public int QuestionId { get; set; }
+        public string? QuestionAudio { get; set; }
+        public string? QuestionParagraph { get; set; }
+        public string? QuestionTranslate { get; set; }
+        public string? QuestionImage { get; set; }
+        public string? CorrectAnswer { get; set; }
+        public string? OptionA { get; set; }
+        public string? OptionB { get; set; }
+        public string? OptionC { get; set; }
+    }
+}
