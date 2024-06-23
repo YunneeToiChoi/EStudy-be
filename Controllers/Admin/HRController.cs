45436284f5d024f204323c80a35262f31aabad84
Te1ne <nq2019thaigiabao230504@gmail.com>
2024-06-23 11:49:58 +0700

    layout marketing hr

---
+++ Controllers/Admin/HRController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace study4_be.Controllers.Admin
+{
+    public class HRController : Controller
+    {
+        public IActionResult HRHome()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Admin/MarketingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace study4_be.Controllers.Admin
+{
+    public class MarketingController : Controller
+    {
+        public IActionResult MarketingHome()
+        {
+            return View();
+        }
+    }
+}
