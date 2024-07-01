1335c2539c375aecf728f77f2c224a215873481c
Te1ne <nq2019thaigiabao230504@gmail.com>
2024-07-01 21:33:21 +0700

    exam

---
+++ Repositories/ExamRepository.cs
@@ -0,0 +1,20 @@
+﻿using study4_be.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace study4_be.Repositories
+{
+    public class ExamRepository
+    {
+        private readonly STUDY4Context _context = new STUDY4Context();
+        public async Task<IEnumerable<Exam>> GetAllExamsAsync()
+        {
+            return await _context.Exams.ToListAsync();
+        }
+        public async Task DeleteAllExamsAsync()
+        {
+            var exams = await _context.Exams.ToListAsync();
+            _context.Exams.RemoveRange(exams);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
