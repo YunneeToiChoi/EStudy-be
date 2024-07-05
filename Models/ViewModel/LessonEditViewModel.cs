f4e56bbf79f383d9dac800b0657cc34cc7a3fc69
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-07-05 20:27:09 +0700

    select lesson edit

---
+++ Models/ViewModel/LessonEditViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace study4_be.Models.ViewModel
+{
+    public class LessonEditViewModel
+    {
+        public Lesson lesson { get; set; }
+        public List<SelectListItem> tags { get; set; }
+    }
+}
