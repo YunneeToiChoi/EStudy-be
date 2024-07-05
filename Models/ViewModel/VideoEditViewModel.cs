8bed953edbb0c483ca007032239b5c7d0a25eea6
Te1ne <nq2019thaigiabao230504@gmail.com>
2024-07-05 21:21:05 +0700

    edit video

---
+++ Models/ViewModel/VideoEditViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace study4_be.Models.ViewModel
+{
+    public class VideoEditViewModel
+    {
+        public Video video { get; set; }
+
+        public List<SelectListItem> lesson { get; set; }
+    }
+}
