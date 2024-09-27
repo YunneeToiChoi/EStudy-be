7c017c1cd393c299771be86201a8d7a07e5fbcad
rena2302 <khoivu2302@gmail.com>
2024-09-27 15:34:17 +0700

    ModelState

--- Controllers/Admin/ExamController.cs
+++ Controllers/Admin/ExamController.cs
@@ -75,6 +75,10 @@ namespace study4_be.Controllers.Admin
         [HttpGet("{id}")]
         public async Task<IActionResult> GetExamById(int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return NotFound(new { message = "Id is invalid" });
+            }
             var exam = await _context.Exams.FindAsync(id);
             if (exam == null)
             {
@@ -86,6 +90,10 @@ namespace study4_be.Controllers.Admin
         [HttpGet]
         public IActionResult Exam_Edit(string id)
         {
+            if (!ModelState.IsValid)
+            {
+                return NotFound(new { message = "examId is invalid" });
+            }
             var exam = _context.Exams.FirstOrDefault(c => c.ExamId == id);
             if (exam == null)
             {
