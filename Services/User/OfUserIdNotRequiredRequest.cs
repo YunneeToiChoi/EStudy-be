39d559b718b35f3482cfdadae05196f6834ca8a4
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-10-11 00:49:30 +0700

    fix outstanding exam guest

---
+++ Services/User/OfUserIdNotRequiredRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace study4_be.Services.User
+{
+    public class OfUserIdNotRequiredRequest
+    {
+        public string? userId { get; set; }
+    }
+}
