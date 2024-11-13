fad6d0e3269541e8d169ac45002f4d252602678a
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-11-13 09:33:42 +0700

    fix avt

--- Services/User/UserUploadImageRequest.cs
+++ Services/User/UserUploadImageRequest.cs
@@ -5,7 +5,7 @@ namespace study4_be.Services.User
     public class UserUploadImageRequest
     {
         public string? userId { get; set; }
-        public IFormFile userAvatar { get; set; }
-        public IFormFile userBanner { get; set; }
+        public IFormFile? userAvatar { get; set; }
+        public IFormFile? userBanner { get; set; }
     }
 }
